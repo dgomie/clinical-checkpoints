ea2e4872b2270aa545cc218cd36ced3d1eb55866
Danny <danny.f.gomez@gmail.com>
2024-08-29 22:18:14 -0400

    rename project clinical-checkpoints, remove placeholder models

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/clinical-notes');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/clinical-checkpoints');
 
 module.exports = mongoose.connection;
--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,3 @@
 const User = require('./User');
-const Legion = require('./Legion')
 
 module.exports = { User, Legion };
