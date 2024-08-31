83f1545d0f2d33733abbc6b763e754f61cb3dbda
Danny <danny.f.gomez@gmail.com>
2024-08-30 21:43:35 -0400

    debug homepage flickering when user logged in

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,31 @@
-import HomeComponent from "../components/HomeComponent";
+import HomeComponent from '../components/HomeComponent';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 function HomePage() {
+  const navigate = useNavigate();
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const isLoggedIn = Auth.loggedIn();
+
+    if (isLoggedIn) {
+      navigate('/dashboard');
+    } else {
+      setAuthChecked(true);
+    }
+  }, [navigate]);
+
+  if (!authChecked) {
+    return null;
+  }
+
   return (
-  <>
-  <HomeComponent />
-  </>)
-  ;
+    <>
+      <HomeComponent />
+    </>
+  );
 }
 
 export default HomePage;
