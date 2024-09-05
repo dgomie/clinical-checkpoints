7fd52a19b37340844881da3d4e075b17ac4fc414
Danny <danny.f.gomez@gmail.com>
2024-09-05 11:05:31 -0400

    add hero img to home page

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,30 +1,40 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import image from "../images/running.jpg";
 
 const HomeComponent = () => {
   const navigate = useNavigate();
 
-
   const handleLoginClick = () => {
-    navigate('/login')
-  }
-  const handleSignUpClick = () => {
-    navigate('/signup')
-  }
+    navigate('/login');
+  };
 
+  const handleSignUpClick = () => {
+    navigate('/signup');
+  };
 
   return (
-    <Container sx={{ paddingBottom: '50px', display: "flex", justifyContent: 'center', alignItems: 'center'  }}>
-
-      <Button variant="contained" sx={{ margin: '1rem' }} onClick={handleLoginClick}>
-        Login
-      </Button>
- 
-      <Button variant="contained" sx={{ margin: '1rem' }} onClick={handleSignUpClick}>
-        Sign Up
-      </Button>
-
+    <Container sx={{ paddingBottom: '50px', display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Box
+        component="img"
+        sx={{
+          width: '100%',
+          height: 'auto',
+          marginBottom: '20px',
+        }}
+        alt="Running"
+        src={image}
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Button variant="contained" sx={{ margin: '1rem' }} onClick={handleLoginClick}>
+          Login
+        </Button>
+        <Button variant="contained" sx={{ margin: '1rem' }} onClick={handleSignUpClick}>
+          Sign Up
+        </Button>
+      </Box>
     </Container>
   );
 };
+
 export default HomeComponent;
