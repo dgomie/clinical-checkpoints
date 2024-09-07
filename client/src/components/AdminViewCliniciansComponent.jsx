614d379311d51605da718524f783573c55389696
Danny <danny.f.gomez@gmail.com>
2024-09-07 19:09:52 -0400

    create admin view clinicians page, update get users query

--- client/src/components/AdminViewCliniciansComponent.jsx
+++ client/src/components/AdminViewCliniciansComponent.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
-import { Paper, Button, Container } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Paper, Container, List, ListItem, ListItemText, Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useNavigate, Link } from "react-router-dom";
+import { useQuery } from '@apollo/client';
+import { GET_USERS } from '../utils/queries';
+import Auth from '../utils/auth'; 
 
 const AdminViewCliniciansComponent = () => {
   const navigate = useNavigate();
+  const { loading, error, data } = useQuery(GET_USERS);
+  const currentUserId = Auth.getProfile().data._id; 
 
-  const handleNavigation = (path) => {
-    navigate(path);
-  };
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const users = data.users.filter(user => user._id !== currentUserId);
+
+
+  const groupedUsers = users.reduce((acc, user) => {
+    const { officeLocation } = user;
+    if (!acc[officeLocation]) {
+      acc[officeLocation] = [];
+    }
+    acc[officeLocation].push(user);
+    return acc;
+  }, {});
 
   return (
-    <Container sx={{ display: 'flex'}}>
+    <Container sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <Paper elevation={3} sx={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '1rem', textAlign:'center'}}>
+        <Typography variant='h5'>View All Clinicians</Typography>
+      </Paper>
       <Paper elevation={3} sx={{ padding: '2rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-        list clinicians
+        {Object.keys(groupedUsers).sort().map(location => (
+          <Accordion key={location}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{location}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <List>
+                {groupedUsers[location].map(user => (
+                  <ListItem 
+                    key={user._id} 
+                    component={Link} 
+                    to={`/admin/edit-user/${user._id}`} 
+                    button
+                  >
+                    <ListItemText 
+                      primary={`${user.firstName} ${user.lastName}`} 
+                      secondary={`Email: ${user.email}`} 
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </AccordionDetails>
+          </Accordion>
+        ))}
       </Paper>
     </Container>
   );
