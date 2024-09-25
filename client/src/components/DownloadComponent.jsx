7f878a90de23d5ec15c185221629177aa0e72d55
Danny <danny.f.gomez@gmail.com>
2024-09-24 22:12:35 -0400

    build out download component, create placeholder for clinician csv download

--- client/src/components/DownloadComponent.jsx
+++ client/src/components/DownloadComponent.jsx
@@ -1,10 +1,38 @@
-import { Container } from '@mui/material';
+import { Container, Paper, Typography } from '@mui/material';
 import CSVComponent from './CSVcomponent';
 
 const DownloadComponent = () => {
-  return <Container>
-    <CSVComponent />
-  </Container>;
+  return (
+    <Container>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          mb: '1rem',
+          textAlign: 'center'
+        }}
+      >
+        <Typography variant='h6'>Download Check Point Progress</Typography>
+        <CSVComponent />
+      </Paper>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '1rem',
+          textAlign: 'center'
+        }}
+      >
+        <Typography variant='h6'>Download Clinician Progress</Typography>
+        <CSVComponent />
+      </Paper>
+    </Container>
+  );
 };
 
 export default DownloadComponent;
