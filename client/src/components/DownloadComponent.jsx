7883a4233727c8b18c4e7033071f6313a031f8f9
Danny <danny.f.gomez@gmail.com>
2024-09-24 22:05:16 -0400

    create download component

---
+++ client/src/components/DownloadComponent.jsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material';
+import CSVComponent from './CSVcomponent';
+
+const DownloadComponent = () => {
+  return <Container>
+    <CSVComponent />
+  </Container>;
+};
+
+export default DownloadComponent;
